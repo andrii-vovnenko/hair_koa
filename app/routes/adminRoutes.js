09d3789f886ed256a394f3d04b118498c2904f23
andriiv <vovnenkoandrii@gmail.com>
2020-06-06 08:36:41 +0300

    add hairTypes

--- app/routes/adminRoutes.js
+++ app/routes/adminRoutes.js
@@ -1,6 +1,8 @@
+const { RED } = require('../../constants/hairType/hairTypeNames');
+
 module.exports = (router) => {
   router
     .get('/admin/index', async (ctx) => {
-      ctx.body = 'Hello';
+      ctx.body = RED;
     })
 };
