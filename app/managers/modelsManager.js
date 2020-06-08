09d04b0c847ed3950fc3b7aa2a22bcbc5a680f98
andriiv <vovnenkoandrii@gmail.com>
2020-06-08 11:23:56 +0300

    fix eer modelname to Unique insert into models with on duplicate key

--- app/managers/modelsManager.js
+++ app/managers/modelsManager.js
@@ -3,7 +3,13 @@ const { knex } = require('../knex');
 const TABLE_NAME = 'models';
 
 const addModel = async (modelData) => {
-  await knex(TABLE_NAME).insert(modelData);
+  return knex.raw(`insert into ${TABLE_NAME} (${Object.keys(modelData).join(', ')})
+    values (${Object.values(modelData).map(v => '?').join(', ')})
+    on duplicate key update ${Object.keys(modelData)
+    .filter(key => key !== 'modelName')
+    .map(key => `${key} = ${modelData[key]}`)
+    .join(', ')};`,
+    Object.values(modelData));
 };
 
 module.exports = {
