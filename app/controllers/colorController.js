93a4c6ecbbbc4bac31f0896f49d2d802b063b656
andriiv <vovnenkoandrii@gmail.com>
2020-06-13 14:05:17 +0300

    fix addColor

--- app/controllers/colorController.js
+++ app/controllers/colorController.js
@@ -2,9 +2,14 @@ const colorsManages = require('../managers/colorsManager');
 
 const addColor = async (ctx) => {
   const { colorName, colorTypeId } = ctx.request.body;
+  if (!colorName || !colorTypeId) return ctx.body = {status: 'failed'}
   await colorsManages.insertColor({ colorName, colorTypeId });
   const colors = await colorsManages.getColors();
-  ctx.body = colors;
+  ctx.body = {
+    entities: {
+      colors,
+    }
+  };
 };
 
 const getColors = async (ctx) => {
