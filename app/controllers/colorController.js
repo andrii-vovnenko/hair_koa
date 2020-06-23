7c8f32927e9bdbb0809037201850095273a975e8
andriiv <vovnenkoandrii@gmail.com>
2020-06-23 21:36:35 +0300

    update creating photos

--- app/controllers/colorController.js
+++ app/controllers/colorController.js
@@ -1,11 +1,14 @@
 const colorsManages = require('../managers/colorsManager');
 
+const delay = (ms) => new Promise((res) => setTimeout(() => res(), ms));
+
 const addColor = async (ctx) => {
   const { colorName, colorTypeId } = ctx.request.body;
   if (!colorName || !colorTypeId) return ctx.body = {status: 'failed'}
   await colorsManages.insertColor({ colorName, colorTypeId });
   const colors = await colorsManages.getColors();
   ctx.body = {
+    status: 'додано',
     entities: {
       colors,
     }
