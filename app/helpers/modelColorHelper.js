1c707c8ef07f836857d83173bcd6f8edbef9fcbd
andriiv <vovnenkoandrii@gmail.com>
2020-08-27 19:07:46 +0300

    update makeModelColorId

--- app/helpers/modelColorHelper.js
+++ app/helpers/modelColorHelper.js
@@ -1,3 +1,4 @@
-const makeModelColorId = ({ modelId, colorId }) => Number(`${modelId}${colorId}`);
+const makeModelColorId = ({ modelId, colorId }) =>
+  `1${String(modelId).padStart(4, '0')}${String(colorId).padStart(4, '0')}`;
 
 module.exports = makeModelColorId;
